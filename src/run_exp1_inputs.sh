9296e1657effa2e83242a06af72918b4e78e9f65
Yuchen Li <yucli@stanford.edu>
2025-03-19 15:48:40 -0700

    Clean up experiment scripts

---
+++ src/run_exp1_inputs.sh
@@ -0,0 +1,8 @@
+# preprocess data
+python3 -m src.preprocessing.preprocess_cesm_data --config src/experiment_configs/exp1_inputs/input3.py
+python3 -m src.preprocessing.preprocess_cesm_data --config src/experiment_configs/exp1_inputs/input4.py
+python3 -m src.preprocessing.preprocess_cesm_data --config src/experiment_configs/exp1_inputs/input5.py
+
+# python3 -m src.preprocessing.preprocess_cesm_data --config src/experiment_configs/config_test_larger_dataset.py
+# python3 -m src.models.train --config src/experiment_configs/config_test_larger_dataset.py
+# python3 -m src.models.evaluate --config src/experiment_configs/config_test_larger_dataset.py
--- src/run_experiment.sh
+++ src/run_experiment.sh
@@ -1,7 +1,4 @@
 
-python3 -m src.preprocessing.preprocess_cesm_data --config src/experiment_configs/config_simple_plus_psl.py
-python3 -m src.models.train --config src/experiment_configs/config_simple_plus_psl.py
-python3 -m src.models.evaluate --config src/experiment_configs/config_simple_plus_psl.py
-
-# python3 -m src.preprocessing.preprocess_cesm_data --config src/experiment_configs/config3.py
-# python3 -m src.models.train --config src/experiment_configs/config3.py 
+python3 -m src.preprocessing.preprocess_cesm_data --config src/experiment_configs/config_test_larger_dataset.py
+python3 -m src.models.train --config src/experiment_configs/config_test_larger_dataset.py
+python3 -m src.models.evaluate --config src/experiment_configs/config_test_larger_dataset.py
