bac303b519874cda18f6e8c5143d4d1118a21841
Yuchen Li <yucli@stanford.edu>
2025-04-02 20:58:42 -0700

    Modify notebooks

--- src/run_exp1_inputs.sh
+++ src/run_exp1_inputs.sh
@@ -3,6 +3,8 @@ python3 -m src.preprocessing.preprocess_cesm_data --config src/experiment_config
 python3 -m src.preprocessing.preprocess_cesm_data --config src/experiment_configs/exp1_inputs/input4.py
 python3 -m src.preprocessing.preprocess_cesm_data --config src/experiment_configs/exp1_inputs/input5.py
 
-# python3 -m src.preprocessing.preprocess_cesm_data --config src/experiment_configs/config_test_larger_dataset.py
-# python3 -m src.models.train --config src/experiment_configs/config_test_larger_dataset.py
+python3 -m src.models.train --config src/experiment_configs/exp1_inputs/input3.py --members 5
+python3 -m src.models.train --config src/experiment_configs/exp1_inputs/input4.py --members 5
+
+
 # python3 -m src.models.evaluate --config src/experiment_configs/config_test_larger_dataset.py
