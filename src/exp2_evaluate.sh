bd8263a3aceabebb4242a19cb5a0a6898ea940c7
Yuchen Li <yucli@stanford.edu>
2025-04-15 11:07:27 -0700

    Add exp2 bash scripts

---
+++ src/exp2_evaluate.sh
@@ -0,0 +1,5 @@
+python3 -m src.models.evaluate --config src/experiment_configs/exp2_data_volume/vol1.py 
+python3 -m src.models.evaluate --config src/experiment_configs/exp2_data_volume/vol2.py 
+python3 -m src.models.evaluate --config src/experiment_configs/exp2_data_volume/vol3.py 
+python3 -m src.models.evaluate --config src/experiment_configs/exp2_data_volume/vol4.py 
+
