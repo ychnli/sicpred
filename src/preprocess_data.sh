e8c94b436b573ecdd78bfdbda33bf000f8b07e70
Yuchen Li <yucli@stanford.edu>
2025-05-04 19:51:03 -0700

    clean up

--- src/preprocess_data.sh
+++
@@ -1,4 +0,0 @@
-
-python3 -m src.preprocessing.preprocess_cesm_data --config src/experiment_configs/config2.py
-
-python3 -m src.preprocessing.preprocess_cesm_data --config src/experiment_configs/config3.py
