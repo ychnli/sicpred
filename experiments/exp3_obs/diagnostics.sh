ff60a17ddfebab165663b5ee62da94febbc753a7
Yuchen Li <yucli@stanford.edu>
2026-04-06 21:10:17 -0700

    Remove overwrite, cleanup notebooks

--- experiments/exp3_obs/diagnostics.sh
+++ experiments/exp3_obs/diagnostics.sh
@@ -6,12 +6,12 @@
 ###################################################################################
 
 # compute diagnostics for pretrained and non-pretrained models
-python3 -m src.models.diagnostics --config src/experiment_configs/exp3_obs/obs_input2.py --overwrite --ensemble-mean --baselines
-python3 -m src.models.diagnostics --config src/experiment_configs/exp3_obs/obs_input2_finetune.py --overwrite --ensemble-mean
+python3 -m src.models.diagnostics --config src/experiment_configs/exp3_obs/obs_input2.py --ensemble-mean --baselines
+python3 -m src.models.diagnostics --config src/experiment_configs/exp3_obs/obs_input2_finetune.py --ensemble-mean
 
 # compute bootstrap confidence intervals for where the diagnostics are different
-python -m src.utils.bootstrap --metric acc --config_a obs_input2_finetune --config_b obs_input2_ensemble --transform fisher_z --overwrite
-python -m src.utils.bootstrap --metric rmse --config_a obs_input2_finetune --config_b obs_input2_ensemble --overwrite
+python -m src.utils.bootstrap --metric acc --config_a obs_input2_finetune --config_b obs_input2_ensemble --transform fisher_z
+python -m src.utils.bootstrap --metric rmse --config_a obs_input2_finetune --config_b obs_input2_ensemble
 
 # run zero-shot evaluations of the pretrained model
 # first, get the location of the model predictions by extracting it from config_cesm.py
@@ -22,5 +22,5 @@ EOF
 )
 PREDICTIONS_PATH="$PREDICTIONS_DIRECTORY/obs_input2_ensemble/exp2_vol4_UNetRes3_zeroshot_predictions.nc"
 
-python3 -m src.models.evaluate --config src/experiment_configs/exp2_data_volume/vol4.py --zero-shot src/experiment_configs/exp3_obs/obs_input2.py --overwrite
-python3 -m src.models.diagnostics --config src/experiment_configs/exp3_obs/obs_input2.py --predictions-path "$PREDICTIONS_PATH" --label _cesm_zs --overwrite
+python3 -m src.models.evaluate --config src/experiment_configs/exp2_data_volume/vol4.py --zero-shot src/experiment_configs/exp3_obs/obs_input2.py
+python3 -m src.models.diagnostics --config src/experiment_configs/exp3_obs/obs_input2.py --predictions-path "$PREDICTIONS_PATH" --label _cesm_zs
